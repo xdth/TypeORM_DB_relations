d99dd062b54a8554ecee087b5bf01664259159b5
dth <dth@dthlabs.com>
2020-09-27 16:52:52 +0200

    CreateOrderService.ts

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,7 +11,7 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
-@Entity('orders_product')
+@Entity('orders_products')
 class OrdersProducts {
   @PrimaryGeneratedColumn('uuid')
   id: string;
